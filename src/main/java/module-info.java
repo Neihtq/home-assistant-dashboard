a5605f79eaa6ee9c35a16d681be3b802c18ba84e
Neihtq <q.thien.nguyen@outlook.de>
2022-10-17 08:34:47 +0200

    implement http GET request

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module neiht.homeassistanthub {
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.net.http;
 
     opens neiht.homeassistanthub to javafx.fxml;
     exports neiht.homeassistanthub;
--- src/main/java/neiht/homeassistanthub/HelloController.java
+++ src/main/java/neiht/homeassistanthub/HelloController.java
@@ -3,7 +3,21 @@ package neiht.homeassistanthub;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 public class HelloController {
+
+    private static final String BASE_URI = "192.168.178.69:8123";
+    private static final HttpClient client = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_1_1)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
     @FXML
     private Label welcomeText;
 
@@ -12,8 +26,32 @@ public class HelloController {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
-    @FXML
-    protected void ontestButtonClick() {
-        System.out.println("Clicked Test");
+    private static HttpRequest getRequest() {
+        return HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(BASE_URI))
+                .setHeader("User-Agent", "Java 11 HttplClient Bot")
+                .build();
     }
+
+    private static void httpRequest(String type) throws IOException, InterruptedException {
+        HttpRequest request;
+        switch (type) {
+            case "GET":
+                request = getRequest();
+                break;
+            default:
+                request = getRequest();
+                break;
+        }
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        HttpHeaders headers = response.headers();
+        headers.map().forEach((k, v) -> System.out.println(k + ':' + v));
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+    }
+
 }
